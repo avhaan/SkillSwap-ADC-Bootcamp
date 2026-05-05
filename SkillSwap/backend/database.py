d1a2ed99edf9ed9f95bffd6b24e7d9c8a9af0874
avhaan <avhaan26@gmail.com>
2026-05-04 23:22:27 -0400

    added new implementation plan

--- SkillSwap/backend/database.py
+++ SkillSwap/backend/database.py
@@ -3,10 +3,9 @@ from pymongo import ReturnDocument
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
+load_dotenv()
 MONGO_URL = os.getenv("MONGO_URL")
-DB_NAME = os.getenv("DB_NAME", "skillswap")
 
 client = AsyncMongoClient(MONGO_URL)
 db = client.skillswap
@@ -16,21 +15,6 @@ reviews_collection = db["reviews"]
 likes_collection = db["likes"]
 
 
+
 async def create_indexes():
-    await users_collection.create_index("email", unique=True)
-    await users_collection.create_index([
-        ("name", "text"),
-        ("skills_offered.name", "text"),
-        ("skills_offered.description", "text"),
-    ])
-    await reviews_collection.create_index(
-        [("reviewer_id", 1), ("target_user_id", 1)],
-        unique=True
-    )
-    await reviews_collection.create_index("target_user_id")
-    await likes_collection.create_index(
-        [("liker_id", 1), ("target_user_id", 1)],
-        unique=True
-    )
-
-
+    await users_collection.create_index("email", unique = True)
