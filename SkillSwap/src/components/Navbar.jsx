ffe7a685f65627d657b376779a43b8f8450b2fe8
Zecarias Demissie <zackdem11@gmail.com>
2026-05-07 17:19:06 -0400

    navbar functionality pt2

--- SkillSwap/src/components/Navbar.jsx
+++ SkillSwap/src/components/Navbar.jsx
@@ -18,14 +18,19 @@ function Navbar() {
       <input className="navbar-search" placeholder="Search skills..." />
 
       <div className="navbar-links">
+        {/* if the user is logged in, we change our navbar to display logout
+            and the profile of the user. */}
         {user ? (
           <>
-             <Link to={`/profile/${user._id}`}>My profile</Link>
+             
              <button onClick={logout} className="navbar-logout">Logout</button>
+             <Link to={`/profile/${user._id}`}>
              <div className="navbar-avatar">{initials}</div>
+             </Link>
           </>
         ) : (
           <>
+        {/* If user is not logged in, we have login, register and browse on the navbar.  */}
         <Link to="/browse">Browse</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
