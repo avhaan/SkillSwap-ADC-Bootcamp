3cf1b248416292b691a20cea8a4dc3524d7ae67a
Zecarias Demissie <zackdem11@gmail.com>
2026-05-07 17:04:59 -0400

    navbar functionality added

--- SkillSwap/src/components/Navbar.jsx
+++ SkillSwap/src/components/Navbar.jsx
@@ -2,8 +2,13 @@
 
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { useAuth } from "../context/AuthContext";
 
 function Navbar() {
+  const {user, logout} = useAuth();
+  const initials = user ? user.name.split(' ').map(n => n[0]).join('') : ''
+
+
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-logo">
@@ -13,9 +18,19 @@ function Navbar() {
       <input className="navbar-search" placeholder="Search skills..." />
 
       <div className="navbar-links">
+        {user ? (
+          <>
+             <Link to={`/profile/${user._id}`}>My profile</Link>
+             <button onClick={logout} className="navbar-logout">Logout</button>
+             <div className="navbar-avatar">{initials}</div>
+          </>
+        ) : (
+          <>
         <Link to="/browse">Browse</Link>
-        <Link to="/profile/me">My profile</Link>
-        <div className="navbar-avatar">MR</div>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        </>
+        )}
       </div>
     </nav>
   );
